0135f91605130b42a261791c2eb0409195221754
xagnetti@adobe.com <xagnetti@adobe.com@a9308255-753e-0410-a2e9-80b3fbc4fff6>
2009-06-05 06:29:44 +0000

    Fix UselessOverridenFunction
    git-svn-id: http://opensource.adobe.com/svn/opensource/flexpmd/trunk@7575 a9308255-753e-0410-a2e9-80b3fbc4fff6

--- flex-pmd-core/src/main/java/com/adobe/ac/pmd/engines/AbstractTestFlexPmdEngine.java
+++ flex-pmd-core/src/main/java/com/adobe/ac/pmd/engines/AbstractTestFlexPmdEngine.java
@@ -87,7 +87,7 @@ public abstract class AbstractTestFlexPmdEngine extends TestCase
                                               new FlexPmdViolations() );
 
       assertEquals( "Number of violations found is not correct",
-                    272,
+                    267,
                     violationsFound );
    }
 
