38a97d82953e0e6124ff57cd1c9894ca68a49872
xagnetti@adobe.com <xagnetti@adobe.com@a9308255-753e-0410-a2e9-80b3fbc4fff6>
2009-06-15 09:34:43 +0000

    fix build
    git-svn-id: http://opensource.adobe.com/svn/opensource/flexpmd/trunk@7833 a9308255-753e-0410-a2e9-80b3fbc4fff6

--- flex-pmd-command-line/src/test/java/com/adobe/ac/pmd/commandline/FlexPMDTest.java
+++ flex-pmd-command-line/src/test/java/com/adobe/ac/pmd/commandline/FlexPMDTest.java
@@ -34,6 +34,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -107,6 +108,6 @@ public class FlexPMDTest extends FlexPmdTestBase
       { "-s",
                   getTestDirectory().getAbsolutePath(),
                   "-o",
-                  "target" } );
+                  new File( "target/test" ).getAbsolutePath() } );
    }
 }
